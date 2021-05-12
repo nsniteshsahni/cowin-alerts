c18ffeefce984823f94469a573693d064f0830cf
nsniteshsahni <niteshsahni20@gmail.com>
2021-05-12 13:00:36 +0530

    Fixing minor bugs

--- cowin-script.js
+++ cowin-script.js
@@ -1,10 +1,10 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const TelegramBot = require("node-telegram-bot-api");
 
-var age18 = false
-var pinCode = process.env.PINCODE || 110085
+var age18 = true
+var pinCode = process.env.PINCODE
 var alerts = 0;
-var token =  process.env.TOKEN
+var token = process.env.TOKEN
 var interval = 10; // seconds
 var intervalID;
 var remainingTime = 10;
@@ -14,26 +14,27 @@ vaccineFilter = true
 var vaccine = "COVAXIN"
 
 var centerAddresses = ["DGD Sector-8 Rohini Delhi", "Sec.5 Rohini, Near Post Office", "A-3 Block, Sector-4 Rohini"]
-
+var centerPreferenceNotRequired = true
 const recheck = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-if(!token){
-  console.log("No Bot Token given, exiting!!!");
-  process.exit(1);
+if (!token) {
+    console.log("No Bot Token given, exiting!!!");
+    process.exit(1);
 }
 
-if(!pinCode){
-    console.log("No pincode specified, using the default: "+ pinCode);
+if (!pinCode) {
+    pinCode = 110085
+    console.log("No pincode specified, using the default: " + pinCode);
 }
 
 function getDate() {
     const d = new Date();
     var month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
-    if (month.length < 2) 
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2)
         month = '0' + month;
-    if (day.length < 2) 
+    if (day.length < 2)
         day = '0' + day;
 
     return [day, month, year].join('-');
@@ -48,14 +49,14 @@ const bot = new TelegramBot(token, { polling: true });
 check()
 
 bot.on("message", async (msg) => {
- var text = msg.text;
-if(text == "start"){
-     alerts = 0;
- }
- else if(text == "kill"){
-    // That means something went wrong, kill the bot.
-    process.exit(1);
- }
+    var text = msg.text;
+    if (text == "start") {
+        alerts = 0;
+    }
+    else if (text == "kill") {
+        // That means something went wrong, kill the bot.
+        process.exit(1);
+    }
 });
 
 function check() {
@@ -66,7 +67,7 @@ function check() {
         checkerForPin()
         intervalID = setInterval(checkerForPin, interval * 1000)
         fetchIntervalId = setInterval(() => {
-            console.log( 'Fetching the slots in ' + remainingTime--);
+            console.log('Fetching the slots in ' + remainingTime--);
         }, 1000)
     }
     running = !running;
@@ -79,7 +80,7 @@ function getByPincode(callback) {
     console.log("Requesting CoWin API for vaccine slots");
     req.open('GET', url, true);
     req.responseType = 'json';
-    req.onload = function() {
+    req.onload = function () {
         var status = req.status;
         if (status === 200)
             callback(null, req.responseText);
@@ -90,21 +91,22 @@ function getByPincode(callback) {
 }
 
 
-async function checkerForPin(){
-    getByPincode(async (err, res) =>{
+async function checkerForPin() {
+    getByPincode(async (err, res) => {
         if (err) return console.log(`Error: ${err}`)
         const age = getAge();
         let count = 0;
         res = JSON.parse(res)
         console.log(res);
         const available = res.centers.filter(center => {
-            if(centerAddresses.includes(center.address)){
-            count += center.sessions[0].min_age_limit === age;
-            return center.sessions.some(s => (s.available_capacity > 0 && s.min_age_limit === age))}
+            if (centerPreferenceNotRequired || centerAddresses.includes(center.address)) {
+                count += center.sessions[0].min_age_limit === age;
+                return center.sessions.some(s => (s.available_capacity > 0 && s.min_age_limit === age))
+            }
         });
         console.log(available);
         await recheck(500)
-        if(available.length!=0) {
+        if (available.length != 0) {
             sendAlert(available);
         }
         remainingTime = interval - 1;
@@ -116,21 +118,21 @@ function sendAlert(available) {
     // Send only the first alert
     if (alerts > 0) return;
     // Send Message to Telegram
-    sendMessageToChannel(available);
     console.log('Found available slots!')
-    alerts ++
+    sendMessageToChannel(available);
+    alerts++
 }
 
-function sendMessageToChannel(available){
+function sendMessageToChannel(available) {
     available.forEach(element => {
         var sessions = []
         element.sessions.forEach(session => {
-           sessions.push({date:session.date, available_capacity: session.available_capacity, vaccine: session.vaccine});
+            sessions.push({ date: session.date, min_age_limit: session.min_age_limit ,available_capacity: session.available_capacity, vaccine: session.vaccine });
         })
-        sessions = vaccineFilter ? sessions.some(session => session.vaccine === vaccine): sessions
-        let slot = {name : element.name, address: element.address, sessions: sessions?sessions:"No eligible vaccine at this center"}
-        if(!(slot.sessions === "No eligible vaccine at this center"))
-           bot.sendMessage("@deathstrokens", JSON.stringify(slot, null, 2));
-           console.log("Sent message to Telegram!!!!");
-        });        
+        sessions = vaccineFilter ? sessions.filter(session => session.vaccine === vaccine) : sessions
+        let slot = { name: element.name, address: element.address, sessions: sessions ? sessions : "No eligible vaccine at this center" }
+        if (!(slot.sessions === "No eligible vaccine at this center"))
+            bot.sendMessage("@deathstrokens", JSON.stringify(slot, null, 2));
+        console.log("Sent message to Telegram!!!!");
+    });
 }
